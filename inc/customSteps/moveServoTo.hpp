da2d6ace52cd4670f73d963266cc196313b08460
Jonas Frei <freij@protonmail.ch>
2022-05-27 09:54:46 +0000

    ✨ Add moveServoTo step

---
+++ inc/customSteps/moveServoTo.hpp
@@ -0,0 +1,33 @@
+#ifndef MOVESERVOTO_HPP_
+#define MOVESERVOTO_HPP_
+
+#include <eeros/sequencer/Step.hpp>
+#include "ControlSystem.hpp"
+
+class MoveServoTo : public eeros::sequencer::Step
+{
+public:
+    MoveServoTo(std::string name, eeros::sequencer::Sequence *caller, ControlSystem &cs)
+        : cs(cs), eeros::sequencer::Step(name, caller)
+    {
+        log.info() << "Step created: " << name;
+    }
+
+    int operator() (double c)
+    {
+        this->c = c;
+        return start();
+    }
+
+    int action()
+    {
+        cs.c.setValue(c);
+        return 0;
+    }
+
+private:
+    ControlSystem &cs;
+    double c;
+};
+
+#endif // MOVESERVOTO_HPP_
